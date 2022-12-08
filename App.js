286e159182632cbfa66b617359e3a68fd3f0bba4
Gabriel Tsujimoto <gabrieltsujimotovicente@outlook.com>
2022-12-08 17:56:54 -0300

    Add assets e src/pages folder

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/screens/Cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Teste de fast Refresh!</Text>
       <StatusBar style="auto" />
+      <Cesta />
     </View>
   );
 }
