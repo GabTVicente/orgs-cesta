c386d83eb2b16aa1b49f54fe1ac2a7aaad35f2f3
GabTVicente <gabrieltsujimotovicente@outlook.com>
2022-12-09 12:52:20 -0300

    inserting fonts

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import {StatusBar, SafeAreaView } from 'react-native';
+import {StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/screens/Cesta';
-
+import {  useFonts } from 'expo-font'
+import { Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [loadedFonts] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold
+  })
+
+  if(!loadedFonts){
+    return <View />
+  }
   return (
     <SafeAreaView>
       <StatusBar animated={true} hidden={true}/>
